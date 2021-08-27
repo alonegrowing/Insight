33de9558bb5208e026d964ff949670ea3a577a35
xuyong <xuyong@xuyongnoMacBook-Pro.local>
2021-08-27 15:40:13 +0800

    Insight 洞察力

---
+++ Insight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Insight
+//
+//  Created by xuyong on 2021/8/27.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
