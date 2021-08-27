553c466c3a8dc52c3d8e258971e53e562b94425a
xuyong <xuyong@xuyongnoMacBook-Pro.local>
2021-08-27 15:10:17 +0800

    Insight 洞察力

---
+++ Insight/InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightApp.swift
+//  Insight
+//
+//  Created by xuyong on 2021/8/27.
+//
+
+import SwiftUI
+
+@main
+struct InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
